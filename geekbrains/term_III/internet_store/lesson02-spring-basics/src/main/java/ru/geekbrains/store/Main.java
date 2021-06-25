03c25f0c00d7144f82a235ab82ae186513547f22
ateresit <ivan.tersintsev@gmail.com>
2021-06-25 19:18:29 +0300

    только приступил к написанию решения ДЗ№ 2. незакончено.

---
+++ geekbrains/term_III/internet_store/lesson02-spring-basics/src/main/java/ru/geekbrains/store/Main.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.store;
+
+public class Main {
+
+    /***
+     *  НЕ УСПЕЛ РЕАЛИЗОВАТЬ В СРОК. ТОЛЬКО ПРИСТУПИЛ К РЕАЛИЗАЦИИ
+     *
+     */
+
+}
---
+++ geekbrains/term_III/internet_store/lesson02-spring-basics/src/main/java/ru/geekbrains/store/persist/ProductRepository.java
@@ -0,0 +1,49 @@
+package ru.geekbrains.store.persist;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * имитация базы данных
+ */
+public class ProductRepository {
+    private final Map<Long, Product> productMap = new ConcurrentHashMap<>();
+    private final AtomicLong idetity = new AtomicLong(0);
+
+    public List<Product> findAll() {
+        return new ArrayList<>(productMap.values());
+    }
+
+    public void insert(Product product) {
+        long id = idetity.incrementAndGet();
+        product.setId(id);
+        productMap.put(id, product);
+    }
+
+    public Product findById(long id) {
+        return productMap.get(id);
+    }
+
+    public void update(Product product) {
+        productMap.put(product.getId(), product);
+    }
+
+    public void delete(long id) {
+        productMap.remove(id);
+    }
+/*
+// формируется ошибка - НЕ НРАВИТСЯ для условия, что LONG == null
+    public void save(Product product) {
+        if (product.getId() == null) {
+            long id = idetity.incrementAndGet();
+            product.setId(id);
+        }
+        productMap.put(product.getId(), product);
+    }
+
+ */
+
+}
