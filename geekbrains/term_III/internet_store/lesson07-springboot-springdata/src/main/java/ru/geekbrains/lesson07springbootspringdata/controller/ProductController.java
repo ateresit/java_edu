99ee6aeac93c0126cd4e89340c869ba90d1c6c7b
ateresit <ivan.tersintsev@gmail.com>
2021-07-16 19:23:02 +0300

    ДЗ к уроку № 08 добавлено

--- geekbrains/term_III/internet_store/lesson07-springboot-springdata/src/main/java/ru/geekbrains/lesson07springbootspringdata/controller/ProductController.java
+++ geekbrains/term_III/internet_store/lesson07-springboot-springdata/src/main/java/ru/geekbrains/lesson07springbootspringdata/controller/ProductController.java
@@ -3,6 +3,9 @@ package ru.geekbrains.lesson07springbootspringdata.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,10 +14,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import ru.geekbrains.lesson07springbootspringdata.persist.Product;
 import ru.geekbrains.lesson07springbootspringdata.persist.ProductRepository;
+import ru.geekbrains.lesson07springbootspringdata.persist.ProductSpecifications;
 
 import javax.validation.Valid;
 import java.math.BigDecimal;
-import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -32,19 +35,36 @@ public class ProductController {
     public String productList(Model model,
                               @RequestParam("productFilter") Optional<String> productFilter,
                               @RequestParam("minCost") Optional<BigDecimal> minCost,
-                              @RequestParam("maxCost") Optional<BigDecimal> maxCost){
+                              @RequestParam("maxCost") Optional<BigDecimal> maxCost,
+                              @RequestParam("page") Optional<Integer> page,
+                              @RequestParam("size") Optional<Integer> size,
+                              @RequestParam("sortField") Optional<String> sortField){
         logger.info("Product list page requested");
 
 /*        List<Product> products = productFilter
                 .map(productRepository::findByProductnameStartsWith
                 .orElseGet(productRepository::findAll));*/
 
-        List<Product> products = productRepository.filterProducts(
+/*        List<Product> products = productRepository.filterProducts(
                 productFilter.orElse(null),
                 minCost.orElse(null),
-                maxCost.orElse(null));
+                maxCost.orElse(null));*/
 
-        model.addAttribute("products", products);
+        Specification<Product> spec = Specification.where(null);
+        if (productFilter.isPresent() && !productFilter.get().isBlank()) {
+            spec = spec.and(ProductSpecifications.productPrefix(productFilter.get()));
+        }
+        if (minCost.isPresent()) {
+            spec = spec.and(ProductSpecifications.minCost(minCost.get()));
+        }
+        if (maxCost.isPresent()) {
+            spec = spec.and(ProductSpecifications.maxCost(maxCost.get()));
+        }
+
+//        model.addAttribute("products", products);
+        model.addAttribute("products", productRepository.findAll(spec,
+                PageRequest.of(page.orElse(1) - 1, size.orElse(3),
+                        Sort.by(sortField.orElse("id")))));
         return "products";
     }
 
--- geekbrains/term_III/internet_store/lesson07-springboot-springdata/src/main/java/ru/geekbrains/lesson07springbootspringdata/persist/ProductRepository.java
+++ geekbrains/term_III/internet_store/lesson07-springboot-springdata/src/main/java/ru/geekbrains/lesson07springbootspringdata/persist/ProductRepository.java
@@ -1,13 +1,14 @@
 package ru.geekbrains.lesson07springbootspringdata.persist;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
 
 //    List<Product> findByProductnameStartsWith(String prefix);
 
