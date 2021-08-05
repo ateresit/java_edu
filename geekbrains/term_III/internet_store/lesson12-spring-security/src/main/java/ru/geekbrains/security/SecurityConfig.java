90271f027430389df03f2e6b1ea0b1f78ccc2257
ateresit <ivan.tersintsev@gmail.com>
2021-08-05 23:28:36 +0300

    Урок 12. Добавлена защита REST API

--- geekbrains/term_III/internet_store/lesson12-spring-security/src/main/java/ru/geekbrains/security/SecurityConfig.java
+++ geekbrains/term_III/internet_store/lesson12-spring-security/src/main/java/ru/geekbrains/security/SecurityConfig.java
@@ -2,13 +2,19 @@ package ru.geekbrains.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.servlet.http.HttpServletResponse;
+
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
     @Autowired
@@ -32,6 +38,32 @@ public class SecurityConfig {
     }
 
     @Configuration
+    @Order(1)
+    public static class ApiWebSecurityConfigAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .antMatcher("/api/**")
+                    .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                    .httpBasic()
+                    .authenticationEntryPoint((reg, resp, exception) -> {
+                        resp.setContentType("application/json");
+                        resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                        resp.setCharacterEncoding("UTF-8");
+                        resp.getWriter().println("{ \"error\": \"" + exception.getMessage() + "\" }");
+                    })
+                    .and()
+                    .csrf().disable()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        }
+    }
+
+    @Configuration
+    @Order(2)
     public static class UiWebSecurityConfigAdapter extends WebSecurityConfigurerAdapter {
 
         @Override
