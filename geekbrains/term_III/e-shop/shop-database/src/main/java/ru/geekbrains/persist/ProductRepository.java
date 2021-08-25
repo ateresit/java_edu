3dc6e0738c3dc21d18429fb2a7582a1a711629eb
ateresit <ivan.tersintsev@gmail.com>
2021-08-25 18:42:13 +0300

    Добавлен код с sql-запросом для устранения проблемы n+1

--- geekbrains/term_III/e-shop/shop-database/src/main/java/ru/geekbrains/persist/ProductRepository.java
+++ geekbrains/term_III/e-shop/shop-database/src/main/java/ru/geekbrains/persist/ProductRepository.java
@@ -1,13 +1,20 @@
 package ru.geekbrains.persist;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import ru.geekbrains.persist.model.Product;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+
+    @Query(value = "select p from Product p left join fetch p.category",
+            countQuery = "select count(p) from Product p")
+    Page<Product> findAll(Pageable var2);
 
     List<Product> findProductByNameLike(String title);
 
