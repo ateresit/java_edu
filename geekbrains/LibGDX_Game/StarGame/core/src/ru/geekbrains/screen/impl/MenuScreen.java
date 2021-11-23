c02e68f0955834d701ef0ad57b5b5a4606aadb9e
ateresit <ivan.tersintsev@gmail.com>
2021-11-23 14:12:19 +0300

    LibGDX StarGame. Правильный код для ДЗ №2, с разбора на уроке 3

--- geekbrains/LibGDX_Game/StarGame/core/src/ru/geekbrains/screen/impl/MenuScreen.java
+++ geekbrains/LibGDX_Game/StarGame/core/src/ru/geekbrains/screen/impl/MenuScreen.java
@@ -8,35 +8,33 @@ import ru.geekbrains.screen.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
 
+    private final float V_LENGTH = 5f;
     private Texture img;
+    private Vector2 position;
     private Vector2 touch;
     private Vector2 v;
-    private Vector2 positionStart;
-    private int xStartPosition = 20;
-    private int yStartPosition = 20;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
+        position = new Vector2();
         touch = new Vector2();
-        positionStart = new Vector2(xStartPosition, yStartPosition);
-        v = new Vector2(1,1);
+        v = new Vector2();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
         batch.begin();
-//        batch.draw(img, touch.x, touch.y);
-        batch.draw(img, positionStart.x, positionStart.y);
-
-        if ((touch.x != positionStart.x || touch.y != positionStart.y) && (positionStart.x <= touch.x && positionStart.y <= touch.y)) {
-//            touch.add(v);
-            positionStart.add(v);
-        }
-
+        batch.draw(img, position.x, position.y);
         batch.end();
+
+        if (touch.dst(position) >= V_LENGTH) {
+            position.add(v);
+        } else {
+            position.set(touch);
+        }
     }
 
     @Override
@@ -48,6 +46,7 @@ public class MenuScreen extends BaseScreen {
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        v.set(touch.cpy().sub(position).setLength(V_LENGTH));
         return super.touchDown(screenX, screenY, pointer, button);
     }
 
