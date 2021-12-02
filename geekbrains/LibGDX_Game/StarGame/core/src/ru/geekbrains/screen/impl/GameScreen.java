50b607dfbf1696aa11b73e11cc1a887a45b11f24
ateresit <ivan.tersintsev@gmail.com>
2021-12-02 19:27:10 +0300

    LibGDX StarGame. ДЗ к уроку №4. Добавлено движение звёзд на игровой экран по нажатию кнопки play

--- geekbrains/LibGDX_Game/StarGame/core/src/ru/geekbrains/screen/impl/GameScreen.java
+++ geekbrains/LibGDX_Game/StarGame/core/src/ru/geekbrains/screen/impl/GameScreen.java
@@ -1,23 +1,36 @@
 package ru.geekbrains.screen.impl;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.math.Rect;
 import ru.geekbrains.screen.BaseScreen;
 import ru.geekbrains.sprite.impl.Background;
+import ru.geekbrains.sprite.impl.Star;
 
 public class GameScreen extends BaseScreen {
 
+    private static final int STAR_COUNT = 64;
+
     private Texture bg;
     private Background background;
+
+    private TextureAtlas atlas;
+    private Star[] stars;
 
     @Override
     public void show() {
         super.show();
         bg = new Texture("dart_vaider_one.jpeg");
         background = new Background(bg);
-        draw();
+        atlas = new TextureAtlas("textures/mainAtlas.tpack");
+
+        stars = new Star[STAR_COUNT];
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star(atlas);
+        }
+//        draw();
     }
 
     @Override
@@ -31,6 +44,9 @@ public class GameScreen extends BaseScreen {
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        for (Star star : stars) {
+            star.resize(worldBounds);
+        }
     }
 
     @Override
@@ -50,12 +66,17 @@ public class GameScreen extends BaseScreen {
     }
 
     private void update(float delta) {
-
+        for (Star star : stars) {
+            star.update(delta);
+        }
     }
 
     private void draw() {
         batch.begin();
         background.draw(batch);
+        for (Star star : stars) {
+            star.draw(batch);
+        }
         batch.end();
 
     }
