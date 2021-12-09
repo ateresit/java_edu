a90f20d73cf49bcb84cd8e1cac6d23f92ad95ab8
ateresit <ivan.tersintsev@gmail.com>
2021-12-09 18:42:52 +0300

    Четверть 4. Архитектура, начальный базовый проект, базовый код WebServer

---
+++ geekbrains/term_IV/architecture-01/study-web-server/src/main/java/ru/geekbrains/WebServer.java
@@ -0,0 +1,69 @@
+package ru.geekbrains;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class WebServer {
+
+    private static String WWW="/Users/macbook/IdeaProjects/first-geek-web-server/www";
+
+    public static void main(String[] args) {
+        try(ServerSocket serverSocket = new ServerSocket(8080)) {
+            System.out.println("Server started!");
+
+            while(true) {
+                Socket socket =serverSocket.accept();
+                System.out.println("New client connected!");
+
+                new Thread(() -> handleRequest(socket)).start();
+            }
+        } catch(IOException e) {
+                e.printStackTrace();
+        }
+    }
+
+    private static void handleRequest(Socket socket) {
+        try(BufferedReader input = new BufferedReader(
+                new InputStreamReader(
+                socket.getInputStream(), StandardCharsets.UTF_8));
+        PrintWriter output = new PrintWriter(socket.getOutputStream())
+        ) {
+            while(!input.ready());
+
+            String firstLine =input.readLine();
+            String[] parts =firstLine.split("");
+            System.out.println(firstLine);
+            while(input.ready()) {
+                System.out.println(input.readLine());
+            }
+
+            Path path =Paths.get(WWW, parts[1]);
+            if(!Files.exists(path)) {
+                output.println("HTTP/1.1 404 NOT_FOUND");
+                output.println("Content-Type: text/html; charset=utf-8");
+                output.println();
+                output.println("<h1>Файл не найден!</h1>");
+                output.flush();
+                return;
+            }
+
+            output.println("HTTP/1.1 200 OK");
+            output.println("Content-Type: text/html; charset=utf-8");
+            output.println();
+
+            Files.newBufferedReader(path).transferTo(output);
+
+            System.out.println("Client disconnected!");
+        } catch(IOException e) {
+                e.printStackTrace();
+        }
+    }
+}
