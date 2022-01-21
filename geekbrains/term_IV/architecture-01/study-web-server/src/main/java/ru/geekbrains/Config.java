0454e41bf5823bdf4371597803366027bcba7f79
ateresit <ivan.tersintsev@gmail.com>
2022-01-21 19:13:15 +0300

    Подготовка ветки для сдачи ДЗ по курсу подготовка к собеседованию

--- geekbrains/term_IV/architecture-01/study-web-server/src/main/java/ru/geekbrains/Config.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains;
-
-public class Config {
-
-    public static final String WWW="/Users/macbook/IdeaProjects/first-geek-web-server/www";
-    public static final int PORT = 8080;
-
-}
