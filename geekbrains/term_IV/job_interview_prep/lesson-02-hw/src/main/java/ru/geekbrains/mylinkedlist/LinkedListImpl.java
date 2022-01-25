7cd149c0bf13adebca316572c5525582f69d8651
ateresit <ivan.tersintsev@gmail.com>
2022-01-25 20:51:01 +0300

    Подготовка к собеседованию. Урок 2, ДЗ 1-основные методы связанного списка

---
+++ geekbrains/term_IV/job_interview_prep/lesson-02-hw/src/main/java/ru/geekbrains/mylinkedlist/LinkedListImpl.java
@@ -0,0 +1,84 @@
+package ru.geekbrains.mylinkedlist;
+
+public class LinkedListImpl<E> implements LinkedList<E> {
+
+    protected Node<E> firstElem;
+    protected int size;
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void insertFirst(E value) {
+        firstElem = new Node<>(value, firstElem);
+        size++;
+    }
+
+    @Override
+    public E getFirst() {
+        if (firstElem != null) {
+            return firstElem.item;
+        }
+        return null;
+    }
+
+    @Override
+    public E removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        Node<E> toRemoveNode = firstElem;
+        firstElem = toRemoveNode.next;
+        toRemoveNode.next = null;
+
+        size--;
+
+        return toRemoveNode.item;
+    }
+
+    @Override
+    public boolean contains(E value) {
+        Node<E> currElem = firstElem;
+        while (currElem != null) {
+            if (currElem.item.equals(value)) {
+                return true;
+            }
+            currElem = currElem.next;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean remove(E value) {
+        return false;
+    }
+
+    @Override
+    public void display() {
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("[");
+
+        Node<E> currElem = firstElem;
+        while (currElem != null) {
+            stringBuilder.append(currElem.item);
+
+            if (currElem.next != null) {
+                stringBuilder.append(" -> ");
+            }
+            currElem = currElem.next;
+        }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+}
---
+++ geekbrains/term_IV/job_interview_prep/lesson-02-hw/src/main/java/ru/geekbrains/mylinkedlist/Main.java
@@ -0,0 +1,33 @@
+package ru.geekbrains.mylinkedlist;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        testMyLinkedList();
+    }
+
+    private static void testMyLinkedList() {
+        LinkedList<Integer> linkedList = new LinkedListImpl<>();
+
+        // проверка добавления
+        linkedList.insertFirst(1);
+        linkedList.insertFirst(2);
+        linkedList.insertFirst(3);
+        linkedList.insertFirst(4);
+        linkedList.insertFirst(5);
+        linkedList.insertFirst(6);
+        linkedList.insertFirst(7);
+        linkedList.insertFirst(8);
+        linkedList.display();
+
+        // проверка поиска
+        System.out.println("Searching 3, result: " + linkedList.contains(3));
+        System.out.println("Searching 7, result: " + linkedList.contains(7));
+        System.out.println("Searching 10, result: " + linkedList.contains(10));
+
+        // проверка удаления
+        linkedList.removeFirst();
+        linkedList.display();
+    }
+}
